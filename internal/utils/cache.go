94eeb633a5fb98fa2e3bb514559fe57528650f98
AvaNdreyWeb <avandrey.web@gmail.com>
2023-07-09 20:25:09 +0300

    Fix cache

--- internal/utils/cache.go
+++ internal/utils/cache.go
@@ -40,7 +40,7 @@ func ClearCachedPages(email string, from, to int) error {
 	for i := from; i <= to; i++ {
 		cachedPage := SearchCache{
 			Email: email,
-			Page:  i + 1,
+			Page:  i,
 		}
 		log.Println("cleaning:", email, "page -", i)
 		key, err := GetHashKey(cachedPage)
